f4f868e05cbbdae1fdcb6c004019d47e7c7ada46
Guangyu Ding <guangyu.ding@okcoin.com>
2018-12-22 20:17:01 +0800

    no cov

--- src/lib/PublicClient.spec.ts
+++
@@ -1,9 +0,0 @@
-// tslint:disable:no-expression-statement
-import test from 'ava';
-import { PublicClient } from './PublicClient';
-
-test('getSpotInstruments', async t => {
-  const publicClient = PublicClient();
-  const data = await publicClient.getSpotInstruments();
-  t.true(data.length > 0);
-});

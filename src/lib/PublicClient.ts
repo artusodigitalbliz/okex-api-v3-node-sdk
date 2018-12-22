a354e42b6b4097959f2dfc5495105386b592c5f5
Kamma <boaiwinserkua@gmail.com>
2018-12-22 19:57:10 +0800

    step 2

--- src/lib/PublicClient.ts
+++ src/lib/PublicClient.ts
@@ -22,9 +22,24 @@ export function PublicClient(
     },
     async getSpotBook(
       instrument_id: string,
-      params: { readonly size: string; readonly depth: string }
+      params?: { readonly size?: string; readonly depth?: string }
     ): Promise<any> {
       return get(`/api/spot/v3/instruments/${instrument_id}/book`, params);
+    },
+    async getSpotTicker(instrument_id?: string): Promise<any> {
+      return instrument_id ? get(`/api/spot/v3/instruments/${instrument_id}/ticker`) : get('/api/spot/v3/instruments/ticker');
+    },
+    async getSpotTrade(
+      instrument_id: string,
+      params?: { readonly from?: string, readonly to?: string, readonly limit?: string }
+    ): Promise<any> {
+      return get(`/api/spot/v3/instruments/${instrument_id}/trades`, params);
+    },
+    async getSpotCandles(
+      instrument_id: string,
+      params?: { readonly start?: string, readonly end?: string, readonly granularity?: number }
+    ): Promise<any> {
+      return get(`/api/spot/v3/instruments/${instrument_id}/candles`, params);
     }
   };
 }

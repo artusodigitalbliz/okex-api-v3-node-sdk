f06435276a9247fffb4c2810fbad2409f8489a84
Guangyu Ding <guangyu.ding@okcoin.com>
2018-12-24 11:35:14 +0800

    现货

--- src/lib/AuthenticatedClient.ts
+++ src/lib/AuthenticatedClient.ts
@@ -2,6 +2,7 @@
 
 import axios, { AxiosInstance } from 'axios';
 import * as crypto from 'crypto';
+import * as querystring from 'querystring';
 
 export function AuthenticatedClient(
   key: string,
@@ -71,8 +72,11 @@ export function AuthenticatedClient(
   }
 
   return {
-    async getSpotAccounts(): Promise<any> {
-      return get('/api/spot/v3/accounts');
+    async getSpotAccounts(currency?: string): Promise<any> {
+      return currency ? get(`/api/spot/v3/accounts/${currency}`) : get('/api/spot/v3/accounts');
+    },
+    async getSpotLedger(currency: string): Promise<any> {
+      return get(`/api/spot/v3/accounts/${currency}/ledger`);
     },
     async postSpotOrder(params: {
       readonly instrument_id: string;
@@ -82,6 +86,54 @@ export function AuthenticatedClient(
       readonly margin_trading?: number;
     }): Promise<any> {
       return post('/api/spot/v3/orders', params);
-    }
+    },
+    async postSpotBatchOrders(params: [{
+      readonly instrument_id: string;
+      readonly client_oid?: string;
+      readonly type: string;
+      readonly side: string;
+      readonly margin_trading?: number;
+    }]): Promise<any> {
+      return post('/api/spot/v3/batch_orders', params);
+    },
+    async postSpotCancelOrder(order_id: string, params: {
+      readonly instrument_d: string,
+      readonly client_oid?: string
+    }): Promise<any> {
+      return post(`/api/spot/v3/cancel_orders/${order_id}`, params);
+    },
+    async postSpotCancelBatchOrders(params: [{ readonly instument_id: string, readonly order_ids: [string] }]): Promise<any> {
+      return post(`/api/spot/v3/cancel_batch_orders`, params);
+    },
+    async getSpotOrders(params: {
+      readonly status: string,
+      readonly instument_id: string,
+      readonly from?: string,
+      readonly to?: string,
+      readonly limit?: string
+    }): Promise<any> {
+      return get(`/api/spot/v3/orders?` + querystring.stringify(params));
+    },
+    async getSpotOrdersPending(params?: {
+      readonly from?: string,
+      readonly to?: string,
+      readonly limit?: string,
+      readonly instrument_id?: string,
+    }): Promise<any> {
+      return get(`/api/spot/v3/orders_pending` + params ? `?${querystring.stringify(params)}` : '');
+    },
+    async getSpotOrder(order_id: string, params: { readonly instrument_id: string }): Promise<any> {
+      return get(`/api/spot/v3/orders/${order_id}?` + querystring.stringify(params));
+    },
+    async getSpotFills(params: {
+      readonly order_id: string,
+      readonly instument_id: string,
+      readonly from?: string,
+      readonly to?: string,
+      readonly limit?: string,
+    }): Promise<any> {
+      return get(`/api/spot/v3/fills?${querystring.stringify(params)}`);
+    },
+
   };
 }

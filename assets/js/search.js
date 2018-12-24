3ed4fe124580d084d04fb37d93b40274b3fd8afb
Guangyu Ding <guangyu.ding@okcoin.com>
2018-12-24 11:39:25 +0800

    [ci skip] Updates

---
+++ assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"64":"Function"},"rows":[{"id":0,"kind":64,"name":"PublicClient","url":"globals.html#publicclient","classes":"tsd-kind-function"},{"id":1,"kind":64,"name":"AuthenticatedClient","url":"globals.html#authenticatedclient","classes":"tsd-kind-function"}]};

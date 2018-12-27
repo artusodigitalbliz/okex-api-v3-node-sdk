a8064f49942f7b737d5f264c881c7b4e77572fb7
oker <vivikcat@msn.com>
2018-12-27 15:17:14 +0800

    feature: api depth checksum

--- src/lib/WebsocketClient.ts
+++ src/lib/WebsocketClient.ts
@@ -3,6 +3,7 @@ import { EventEmitter } from 'events';
 import WebSocket from 'ws';
 import pako from 'pako';
 import crypto from 'crypto';
+import crc32 from 'crc-32';
 
 export class V3WebsocketClient extends EventEmitter {
   private websocketUri: string;
@@ -51,6 +52,37 @@ export class V3WebsocketClient extends EventEmitter {
     this.send({ op: 'unsubscribe', args });
   }
 
+  checksum(data: any) {
+    if (data == null || data == undefined) {
+      return false;
+    }
+    let result = data;
+    if (typeof data === 'string') {
+      result = JSON.parse(data);
+    }
+    if (result.data && result.data.length > 0) {
+      const item = result.data[0];
+      const buff = [];
+      for (let i = 0; i < 25; i++) {
+        if (item.bids[i]) {
+          const bid = item.bids[i];
+          buff.push(bid[0]);
+          buff.push(bid[1]);
+        }
+        if (item.asks[i]) {
+          const ask = item.asks[i];
+          buff.push(ask[0]);
+          buff.push(ask[1]);
+        }
+      }
+      const checksum = crc32.str(buff.join(':'));
+      if (checksum === item.checksum) {
+        return true;
+      }
+    }
+    return false;
+  }
+
   private send(messageObject: any) {
     if (!this.socket) throw Error('socket is not open');
     this.socket.send(JSON.stringify(messageObject));
@@ -80,7 +112,7 @@ export class V3WebsocketClient extends EventEmitter {
 
   private onMessage(data: WebSocket.Data) {
     this.resetTimer();
-    if (!(data instanceof String)) {
+    if (!(typeof data === 'string')) {
       data = pako.inflateRaw(data as any, { to: 'string' });
     }
     if (data === 'pong') {
